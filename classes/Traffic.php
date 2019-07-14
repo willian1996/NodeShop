b5995f7c0aa556b1035df823a51b56c79ed1bdf2
Willian Sales <williansalesgabriel@hotmail.com>
2019-07-14 10:35:40 -0300

    Finalizando a classe Traffic

--- classes/Traffic.php
+++ classes/Traffic.php
@@ -6,6 +6,7 @@ class Traffic{
     private $ip;
     private $data;
     private $uri;
+    private $user_agent;
     
     public function __construct(){
         $this->db = new PDO("mysql:host=localhost;dbname=bd_grafico;charset=utf8", "root", "");
@@ -13,21 +14,20 @@ class Traffic{
         $this->uri = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_DEFAULT);
         $this->ip = '177.137.32.58';//filter_input(INPUT_SERVER, 'REMOTE_ADDR', FILTER_VALIDATE_IP);
         $cookie = filter_input(INPUT_COOKIE, md5($this->uri), FILTER_DEFAULT);
+        $this->user_agent = filter_input(INPUT_SERVER, 'HTTP_USER_AGENT');
         
-//        if(!$cookie){
-//            $this->_set_cookie();   
-//        }
-        
-        $this->_rec_data();
-        
-        
+        if(!$cookie){
+            $this->_set_cookie();   
+            $this->_set_data();
+        }
+
     }
     
     private function _set_cookie(){
-        setcookie(md5($this->uri, TRUE, strtotime(date('Y-m-d 23:59:59'))));
+        md5(setcookie($this->uri, TRUE, strtotime(date('Y-m-d 23:59:59'))));
     }
     
-    private function _rec_data(){
+    private function _set_data(){
         
         //requisição json de geolocalização
         $url = "http://ip-api.com/json/{$this->ip}";
@@ -47,12 +47,58 @@ class Traffic{
         
         $this->data['data'] = date('Y-m-d H:i:s');
         $this->data['pagina'] = $this->uri;
+        $this->data['ip'] = $this->ip;
         $this->data['cidade'] = (isset($geo->city)) ? $geo->city : 'Desconhecida';
         $this->data['regiao'] = (isset($geo->regionName)) ? $geo->regionName : 'Desconhecida';
         $this->data['pais'] = (isset($geo->country)) ? $geo->country : 'Desconhecida';
+        $this->data['navegador'] = $this->_get_browser();
+        $this->data['plataforma'] = $this->_get_platform();
+        $this->data['referencia'] = $this->_get_referer();
         
-        var_dump($this->data);
-        die;
+        $this->_rec_data();
+    }
+    
+    private function _get_browser(){
+        require_once 'config/browsers.php';
+        foreach($browsers as $key => $value){
+            if(preg_match('|' . $key . '.*?([0-9\.]+)|i', $this->user_agent)){
+                return $value;
+            } 
+        }
+    }
+    
+    private function _get_platform(){
+        require_once 'config/platforms.php';
+        foreach($platforms as $key => $value){
+            if(preg_match('|' . preg_quote($key). '|i', $this->user_agent)){
+                return $value;
+            } 
+        }
+    }
+    
+    private function _get_referer(){
+        $referer = filter_input(INPUT_SERVER, 'HTTP_REFERER', FILTER_VALIDATE_URL);
+        $referer_host = parse_url($referer, PHP_URL_HOST);
+        $host = filter_input(INPUT_SERVER, 'SERVER_NAME');
+        
+        
+        if(!$referer){
+            $retorno = 'Acesso direto';
+        }elseif($referer_host == $host){
+            $retorno = 'Navegação Interna';
+        }else{
+            $retorno = $referer;
+        }
+        
+        return $retorno;
+    }
+    
+    private function _rec_data(){
+        $sql = "INSERT INTO trafego (data, pagina, ip, cidade, regiao, pais, navegador, referencia, plataforma)"
+              ."VALUES (:data, :pagina, :ip, :cidade, :regiao, :pais, :navegador, :referencia, :plataforma)";
+        $query = $this->db->prepare($sql);
+        $query->execute($this->data);
+        
         
     }
     
